8c969f1cc0aa87e5fef8ccf5e2039129d02e9c11
Oscar Zheng <Oscarzheng2016@gmail.com>
2022-11-16 17:35:21 -0600

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+let toTop= document.getElementById("to-top");
+window.onscroll = function () {
+    scroll();
+    };
+function scroll() {
+    if(document.body.scrollTop>20 ||
+        document.documentElement.scrollTop > 20){
+            toTop.style.display="block";
+        }else {
+            toTop.style.display="none";
+        }
+}
+toTop.addEventListener("click", top );
+function top(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
