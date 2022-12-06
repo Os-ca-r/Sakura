258e10a714087ee51f22d6c464faeef0da7d39ab
Oscar Zheng <Oscarzheng2016@gmail.com>
2022-12-06 13:41:19 -0600

    style

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const up= document.querySelector("#toTop");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    up.style.display="block"
-  } else {
-    up.style.display="none"
-  }
-})
+<button onclick="topFunction()" id="backtoTop" title="Go to top">Top</button>
