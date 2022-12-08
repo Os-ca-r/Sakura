df2658cdcd7934098f6b232c2e63ecaa13eaf519
Oscar Zheng <Oscarzheng2016@gmail.com>
2022-12-07 21:38:58 -0600

    back to top button

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 // Get the button
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("backtotop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20|| document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
