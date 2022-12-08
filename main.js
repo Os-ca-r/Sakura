2c2f85b7dfbd69ee8470514cd6e73d5e70435e42
kevin olvera <Kd5318530@gmail.com>
2022-12-08 11:57:19 -0600

    push

--- main.js
+++ main.js
@@ -32,3 +32,28 @@ document.getElementById("orderItems").appendChild(clone)
 
 }
 
+var div = document.getElementById('Order');
+
+function addOrder() {
+  var input = document.createElement('input'),
+    button = document.createElement('button');
+  
+  input.placeholder = "Order";
+  button.innerHTML = 'X';
+  // attach onlick event handler to remove button
+  button.onclick = removeOrder;
+  
+  div.appendChild(input);
+  div.appendChild(button);
+}
+
+function removeOrder() {
+  // remove this button and its input
+  div.removeChild(this.previousElementSibling);
+  div.removeChild(this);
+}
+
+// attach onclick event handler to add button
+document.getElementById('add').addEventListener('click', addOrder);
+// attach onclick event handler to 1st remove button
+document.getElementById('remove').addEventListener('click', removeOrder);
